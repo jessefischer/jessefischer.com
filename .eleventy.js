ab670c8b6103a2b944c0c010c54d9aa3bbf5151a
Jesse Fischer <59373483+jessefischer@users.noreply.github.com>
2021-06-03 13:12:41 -0400

    Added Netlify CMS

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function( eleventyConfig ) {
 	eleventyConfig.addPassthroughCopy( "src/img" );
 	eleventyConfig.addPassthroughCopy( "src/js" );
 	eleventyConfig.addPassthroughCopy( "src/css" );
+	eleventyConfig.addPassthroughCopy( "src/admin" );
 	eleventyConfig.addPassthroughCopy( "data" );
 
 	eleventyConfig.setBrowserSyncConfig( {
